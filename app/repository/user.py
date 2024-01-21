70f032defb69bc5577263a37f08e39a416d2a05c
mikaijun <mj.tempe443@gmail.com>
2024-01-21 14:26:59 +0900

    userのリポジトリをclass化

---
+++ app/repository/user.py
@@ -0,0 +1,22 @@
+from firebase_admin import firestore
+from model.User import User
+from google.cloud.firestore import FieldFilter
+
+
+class UserRepository:
+    def __init__(self):
+        self.db = firestore.client()
+        self.users_ref = self.db.collection("users")
+
+    def create_user(self, user: User):
+        doc_ref = self.users_ref.document(user.id)
+        doc_ref.set({"id": user.id})
+        return User(id=user.id)
+
+    def find_user_by_uid(self, user: User):
+        query_ref = self.users_ref.where(filter=FieldFilter("id", "==", user.id))
+        result = query_ref.get()
+        if 0 < len(result):
+            return User(id=result[0].id)
+        else:
+            return None
--- app/routers/users.py
+++ app/routers/users.py
@@ -2,10 +2,11 @@ from typing import List, Union
 from fastapi import APIRouter, Depends, HTTPException, Header
 from model.Authorization import Authorization
 from repository.Authorization import get_current_user, verify_id_token
-from repository.users import create_user, find_user_by_uid
+from repository.user import UserRepository
 from model.User import User
 
 router = APIRouter()
+user_repository = UserRepository()
 
 
 @router.get("/user", tags=["users"])
@@ -25,10 +26,10 @@ def user_create(x_token: Union[List[str], None] = Header(default=None)):
             detail="ユーザー認証に失敗しました",
         )
     new_user = User.create(verified_token.uid)
-    already_user = find_user_by_uid(new_user)
+    already_user = user_repository.find_user_by_uid(new_user)
     if already_user is not None:
         raise HTTPException(
             status_code=409,
             detail="既に登録済みのユーザーです",
         )
-    return create_user(new_user)
+    return user_repository.create_user(new_user)
