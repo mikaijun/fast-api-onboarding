aff7907f7add0d05bb0ba04452be67617a95c0f9
mikaijun <mj.tempe443@gmail.com>
2024-01-21 14:13:51 +0900

    変数名変更

--- app/routers/users.py
+++ app/routers/users.py
@@ -24,10 +24,11 @@ def user_create(x_token: Union[List[str], None] = Header(default=None)):
             status_code=401,
             detail="ユーザー認証に失敗しました",
         )
-    user = find_user_by_uid(User.create(verified_token.uid))
-    if user is not None:
+    new_user = User.create(verified_token.uid)
+    already_user = find_user_by_uid(new_user)
+    if already_user is not None:
         raise HTTPException(
             status_code=409,
             detail="既に登録済みのユーザーです",
         )
-    return create_user(User.create(verified_token.uid))
+    return create_user(new_user)
